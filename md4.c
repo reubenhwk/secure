8482f15f166ab0a1cffc1a032c190d6eb59f257e
Reuben Hawkins <reubenhwk@gmail.com>
2015-07-24 16:34:15 -0700

    loop in Update

--- md4.c
+++ md4.c
@@ -37,7 +37,7 @@ static inline uint32_t rol(uint32_t x, int y)
 	return (x << y) | (x >> (32-y));
 }
 
-static void rounds(md4_t * md, uint32_t const * restrict b)
+static inline void rounds(md4_t * md, uint32_t const * restrict b, size_t count)
 {
 
 #define ff(x, y, z) (x&y | (~x)&z)
@@ -45,7 +45,7 @@ static void rounds(md4_t * md, uint32_t const * restrict b)
 #define hh(x, y, z) (x ^ y ^ z)
 
 #define r1(A, B, C, D, i, s) (A = rol(A + ff(B, C, D) + b[i], s))
-#define r2(A, B, C, D, i, s) (A = rol(A + gg(B, C, D) + b[i] + 0x5a827999, s))
+#define r2(A, B, C, D, i, s) (A = rol(A + gg(B, C, D) + b[i] + 0x5A827999, s))
 #define r3(A, B, C, D, i, s) (A = rol(A + hh(B, C, D) + b[i] + 0x6ED9EBA1, s))
 
 	uint32_t A = md->s._32[0];
@@ -53,61 +53,63 @@ static void rounds(md4_t * md, uint32_t const * restrict b)
 	uint32_t C = md->s._32[2];
 	uint32_t D = md->s._32[3];
 
-	r1(A, B, C, D,  0,  3);
-	r1(D, A, B, C,  1,  7);
-	r1(C, D, A, B,  2, 11);
-	r1(B, C, D, A,  3, 19);
-	r1(A, B, C, D,  4,  3);
-	r1(D, A, B, C,  5,  7);
-	r1(C, D, A, B,  6, 11);
-	r1(B, C, D, A,  7, 19);
-	r1(A, B, C, D,  8,  3);
-	r1(D, A, B, C,  9,  7);
-	r1(C, D, A, B, 10, 11);
-	r1(B, C, D, A, 11, 19);
-	r1(A, B, C, D, 12,  3);
-	r1(D, A, B, C, 13,  7);
-	r1(C, D, A, B, 14, 11);
-	r1(B, C, D, A, 15, 19);
+	while (count--) {
+		r1(A, B, C, D,  0,  3);
+		r1(D, A, B, C,  1,  7);
+		r1(C, D, A, B,  2, 11);
+		r1(B, C, D, A,  3, 19);
+		r1(A, B, C, D,  4,  3);
+		r1(D, A, B, C,  5,  7);
+		r1(C, D, A, B,  6, 11);
+		r1(B, C, D, A,  7, 19);
+		r1(A, B, C, D,  8,  3);
+		r1(D, A, B, C,  9,  7);
+		r1(C, D, A, B, 10, 11);
+		r1(B, C, D, A, 11, 19);
+		r1(A, B, C, D, 12,  3);
+		r1(D, A, B, C, 13,  7);
+		r1(C, D, A, B, 14, 11);
+		r1(B, C, D, A, 15, 19);
 
-	r2(A, B, C, D,  0,  3);
-	r2(D, A, B, C,  4,  5);
-	r2(C, D, A, B,  8,  9);
-	r2(B, C, D, A, 12, 13);
-	r2(A, B, C, D,  1,  3);
-	r2(D, A, B, C,  5,  5);
-	r2(C, D, A, B,  9,  9);
-	r2(B, C, D, A, 13, 13);
-	r2(A, B, C, D,  2,  3);
-	r2(D, A, B, C,  6,  5);
-	r2(C, D, A, B, 10,  9);
-	r2(B, C, D, A, 14, 13);
-	r2(A, B, C, D,  3,  3);
-	r2(D, A, B, C,  7,  5);
-	r2(C, D, A, B, 11,  9);
-	r2(B, C, D, A, 15, 13);
+		r2(A, B, C, D,  0,  3);
+		r2(D, A, B, C,  4,  5);
+		r2(C, D, A, B,  8,  9);
+		r2(B, C, D, A, 12, 13);
+		r2(A, B, C, D,  1,  3);
+		r2(D, A, B, C,  5,  5);
+		r2(C, D, A, B,  9,  9);
+		r2(B, C, D, A, 13, 13);
+		r2(A, B, C, D,  2,  3);
+		r2(D, A, B, C,  6,  5);
+		r2(C, D, A, B, 10,  9);
+		r2(B, C, D, A, 14, 13);
+		r2(A, B, C, D,  3,  3);
+		r2(D, A, B, C,  7,  5);
+		r2(C, D, A, B, 11,  9);
+		r2(B, C, D, A, 15, 13);
 
-	r3(A, B, C, D,  0,  3);
-	r3(D, A, B, C,  8,  9);
-	r3(C, D, A, B,  4, 11);
-	r3(B, C, D, A, 12, 15);
-	r3(A, B, C, D,  2,  3);
-	r3(D, A, B, C, 10,  9);
-	r3(C, D, A, B,  6, 11);
-	r3(B, C, D, A, 14, 15);
-	r3(A, B, C, D,  1,  3);
-	r3(D, A, B, C,  9,  9);
-	r3(C, D, A, B,  5, 11);
-	r3(B, C, D, A, 13, 15);
-	r3(A, B, C, D,  3,  3);
-	r3(D, A, B, C, 11,  9);
-	r3(C, D, A, B,  7, 11);
-	r3(B, C, D, A, 15, 15);
+		r3(A, B, C, D,  0,  3);
+		r3(D, A, B, C,  8,  9);
+		r3(C, D, A, B,  4, 11);
+		r3(B, C, D, A, 12, 15);
+		r3(A, B, C, D,  2,  3);
+		r3(D, A, B, C, 10,  9);
+		r3(C, D, A, B,  6, 11);
+		r3(B, C, D, A, 14, 15);
+		r3(A, B, C, D,  1,  3);
+		r3(D, A, B, C,  9,  9);
+		r3(C, D, A, B,  5, 11);
+		r3(B, C, D, A, 13, 15);
+		r3(A, B, C, D,  3,  3);
+		r3(D, A, B, C, 11,  9);
+		r3(C, D, A, B,  7, 11);
+		r3(B, C, D, A, 15, 15);
 
-	md->s._32[0] += A;
-	md->s._32[1] += B;
-	md->s._32[2] += C;
-	md->s._32[3] += D;
+		A = md->s._32[0] += A;
+		B = md->s._32[1] += B;
+		C = md->s._32[2] += C;
+		D = md->s._32[3] += D;
+	}
 }
 
 void MD4_Update(md4_t * md, unsigned char const * d, size_t len)
@@ -117,17 +119,17 @@ void MD4_Update(md4_t * md, unsigned char const * d, size_t len)
 
 	if (left < len) {
 		memcpy(&md->b._8[used], d, left);
-		rounds(md, md->b._32);
+		rounds(md, md->b._32, 1);
 		d += left;
 		md->count += left;
 		len -= left;
 	}
 
-	while (len >= 0x40) {
-		rounds(md, (uint32_t *)d);
-		d += 0x40;
-		len -= 0x40;
-		md->count += 0x40;
+	if (len >= 64) {
+		rounds(md, (uint32_t *)d, (len / 0x40));
+		d += 0x40 * (len / 0x40);
+		len -= 0x40 * (len / 0x40);
+		md->count += 0x40 * (len / 0x40);
 	}
 
 	memcpy(&md->b._8[0], d, len);
