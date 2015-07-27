048ba8d6aea26c930a76a30dc8a0969f3b6c2030
Reuben Hawkins <reubenhwk@gmail.com>
2015-07-26 22:12:44 -0700

    remove MD4/MD5_Init (use common MD_Init)

--- md/md.c
+++ md/md.c
@@ -4,6 +4,34 @@
 #include <string.h>
 #include <stdio.h>
 
+md_t * MD_Init(md_t * md)
+{
+	if (!md) {
+		md = malloc(sizeof(md_t));
+	}
+
+	md->s._8[0x0] = 0x01;
+	md->s._8[0x1] = 0x23;
+	md->s._8[0x2] = 0x45;
+	md->s._8[0x3] = 0x67;
+	md->s._8[0x4] = 0x89;
+	md->s._8[0x5] = 0xab;
+	md->s._8[0x6] = 0xcd;
+	md->s._8[0x7] = 0xef;
+	md->s._8[0x8] = 0xfe;
+	md->s._8[0x9] = 0xdc;
+	md->s._8[0xa] = 0xba;
+	md->s._8[0xb] = 0x98;
+	md->s._8[0xc] = 0x76;
+	md->s._8[0xd] = 0x54;
+	md->s._8[0xe] = 0x32;
+	md->s._8[0xf] = 0x10;
+
+	md->count = 0;
+
+	return md;
+}
+
 void MD_Update(md_t * md, unsigned char const * d, size_t len, MD_ROUNDS_PROC rounds)
 {
 	size_t used = (md->count & 0x3f);
--- md5/md5.c
+++ md5/md5.c
@@ -4,34 +4,6 @@
 #include <string.h>
 #include <stdio.h>
 
-md5_t * MD5_Init(md5_t * md)
-{
-	if (!md) {
-		md = malloc(sizeof(md5_t));
-	}
-
-	md->s._8[0x0] = 0x01;
-	md->s._8[0x1] = 0x23;
-	md->s._8[0x2] = 0x45;
-	md->s._8[0x3] = 0x67;
-	md->s._8[0x4] = 0x89;
-	md->s._8[0x5] = 0xab;
-	md->s._8[0x6] = 0xcd;
-	md->s._8[0x7] = 0xef;
-	md->s._8[0x8] = 0xfe;
-	md->s._8[0x9] = 0xdc;
-	md->s._8[0xa] = 0xba;
-	md->s._8[0xb] = 0x98;
-	md->s._8[0xc] = 0x76;
-	md->s._8[0xd] = 0x54;
-	md->s._8[0xe] = 0x32;
-	md->s._8[0xf] = 0x10;
-
-	md->count = 0;
-
-	return md;
-}
-
 static inline uint32_t rol(uint32_t x, int y)
 {
 	return (x << y) | (x >> (32-y));
