8792c577ed39fb69ee05d5b8b7c19c1831d48c60
Reuben Hawkins <reubenhwk@gmail.com>
2015-08-18 10:01:31 -0700

    reset chacha20 test, remove chacha[8|12] tests

--- test/chacha20.sh
+++ test/chacha20.sh
@@ -2,7 +2,7 @@
 
 v1=$(cat test/chacha20-test.bin | ./cpt chacha20 | ./cpt md5)
 
-if [ "$v1" != "77bdfe19163879436d9ec347674d651a" ] ; then
+if [ "$v1" != "6e87d4ecf5d27ff96090f4616ecd3f5c" ] ; then
 	echo "vector1 failed" >&2
 	exit -1
 fi
