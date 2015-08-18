a407807f74ab7d224280c7b180111778a070aefe
Reuben Hawkins <reubenhwk@gmail.com>
2015-08-18 10:01:54 -0700

    add the chacha8, chacha12 tests back

--- test/chacha8.sh
+++ test/chacha8.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-v1=$(cat test/chacha20-test.bin | ./cpt chacha20 | ./cpt md5)
+v1=$(cat test/chacha8-test.bin | ./cpt chacha8 | ./cpt md5)
 
 if [ "$v1" != "f7408430cc526210deccbf6a0c67e623" ] ; then
 	echo "vector1 failed" >&2
