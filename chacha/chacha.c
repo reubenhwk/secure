d2444260529c037d3bd38736f6e83226429c3978
Reuben Hawkins <reubenhwk@gmail.com>
2015-08-18 10:01:20 -0700

    chacha code cleanup

--- chacha/chacha.c
+++ chacha/chacha.c
@@ -9,19 +9,26 @@
 #define ROTATE(v, c) ROTL32((v), (c))
 #define XOR(v, w) ((v) ^ (w))
 #define PLUS(x, y) ((x) + (y))
-#define U32TO8_LITTLE(p, v) \
-{ (p)[0] = ((v)      ) & 0xff; (p)[1] = ((v) >>  8) & 0xff; \
-(p)[2] = ((v) >> 16) & 0xff; (p)[3] = ((v) >> 24) & 0xff; }
 
-#define U8TO32_LITTLE(p)   \
-(((uint32_t)((p)[0])      ) | ((uint32_t)((p)[1]) <<  8) | \
-((uint32_t)((p)[2]) << 16) | ((uint32_t)((p)[3]) << 24)   )
+#define U32TO8_LITTLE(p, v) do { \
+	(p)[0] = ((v)      ) & 0xff; \
+	(p)[1] = ((v) >>  8) & 0xff; \
+	(p)[2] = ((v) >> 16) & 0xff; \
+	(p)[3] = ((v) >> 24) & 0xff; \
+} while (0)
 
-#define QUARTERROUND(a,b,c,d) \
-x[a] = PLUS(x[a],x[b]); x[d] = ROTATE(XOR(x[d],x[a]),16); \
-x[c] = PLUS(x[c],x[d]); x[b] = ROTATE(XOR(x[b],x[c]),12); \
-x[a] = PLUS(x[a],x[b]); x[d] = ROTATE(XOR(x[d],x[a]), 8); \
-x[c] = PLUS(x[c],x[d]); x[b] = ROTATE(XOR(x[b],x[c]), 7);
+#define U8TO32_LITTLE(p) \
+	(((uint32_t)((p)[0])      ) | \
+	 ((uint32_t)((p)[1]) <<  8) | \
+	 ((uint32_t)((p)[2]) << 16) | \
+	 ((uint32_t)((p)[3]) << 24) )
+
+#define QUARTERROUND(a,b,c,d) do { \
+	x[a] = PLUS(x[a],x[b]); x[d] = ROTATE(XOR(x[d],x[a]),16); \
+	x[c] = PLUS(x[c],x[d]); x[b] = ROTATE(XOR(x[b],x[c]),12); \
+	x[a] = PLUS(x[a],x[b]); x[d] = ROTATE(XOR(x[d],x[a]), 8); \
+	x[c] = PLUS(x[c],x[d]); x[b] = ROTATE(XOR(x[b],x[c]), 7); \
+} while (0)
 
 static void ChaChaCore(unsigned char output[64], const uint32_t input[16], int num_rounds)
 {
@@ -31,18 +38,18 @@ static void ChaChaCore(unsigned char output[64], const uint32_t input[16], int n
 	memcpy(x, input, sizeof(uint32_t) * 16);
 
 	for (i = num_rounds; i > 0; i -= 2) {
-		QUARTERROUND( 0, 4, 8,12)
-		QUARTERROUND( 1, 5, 9,13)
-		QUARTERROUND( 2, 6,10,14)
-		QUARTERROUND( 3, 7,11,15)
-		QUARTERROUND( 0, 5,10,15)
-		QUARTERROUND( 1, 6,11,12)
-		QUARTERROUND( 2, 7, 8,13)
-		QUARTERROUND( 3, 4, 9,14)
+		QUARTERROUND( 0, 4, 8,12);
+		QUARTERROUND( 1, 5, 9,13);
+		QUARTERROUND( 2, 6,10,14);
+		QUARTERROUND( 3, 7,11,15);
+		QUARTERROUND( 0, 5,10,15);
+		QUARTERROUND( 1, 6,11,12);
+		QUARTERROUND( 2, 7, 8,13);
+		QUARTERROUND( 3, 4, 9,14);
 	}
 
 	for (i = 0; i < 16; ++i) {
-		x[i] = PLUS(x[i], input[i]);
+		x[i] += input[i];
 		U32TO8_LITTLE(output + 4 * i, x[i]);
 	}
 }
