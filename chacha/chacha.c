870a62acf6e190ca68baee3685e05b32b9eb19c0
Reuben Hawkins <reubenhwk@gmail.com>
2015-08-18 10:01:48 -0700

    chacha.c: significant cleanup

--- chacha/chacha.c
+++ chacha/chacha.c
@@ -5,25 +5,21 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef union {
+	uint8_t u8[64];
+	uint32_t u32[16];
+	uint64_t u64[8];
+} matrix_t;
+
+typedef struct {
+	matrix_t matrix;
+	matrix_t block;
+	int used;
+	int rounds;
+} chacha_ctx_t;
+
 #define ROTL32(v, n) (((v) << (n)) | ((v) >> (32 - (n))))
 #define ROTATE(v, c) ((v) = ROTL32((v), (c)))
-
-#ifdef BIGE
-#define U32TO8_LITTLE(p, v) do { \
-	(p)[0] = ((v)      ) & 0xff; \
-	(p)[1] = ((v) >>  8) & 0xff; \
-	(p)[2] = ((v) >> 16) & 0xff; \
-	(p)[3] = ((v) >> 24) & 0xff; \
-} while (0)
-#define U8TO32_LITTLE(p) \
-	(((uint32_t)((p)[0])      ) | \
-	 ((uint32_t)((p)[1]) <<  8) | \
-	 ((uint32_t)((p)[2]) << 16) | \
-	 ((uint32_t)((p)[3]) << 24) )
-#else
-#define U32TO8_LITTLE(p, v) ((((uint32_t*)(p))[0]) = (((uint32_t)(v))))
-#define U8TO32_LITTLE(p) (((uint32_t*)(p))[0])
-#endif
 
 static inline void chacha_round(uint32_t x[16], int a, int b, int c, int d)
 {
@@ -45,37 +41,18 @@ static inline void chacha_rounds(uint32_t x[16])
 	chacha_round(x, 3, 4, 9,14);
 }
 
-static void ChaChaCore(unsigned char output[64], const uint32_t matrix[16], int num_rounds)
+static void ChaChaCore(chacha_ctx_t * ctx, int num_rounds)
 {
-	uint32_t x[16];
-
-	memcpy(x, matrix, sizeof(uint32_t) * 16);
+	ctx->block = ctx->matrix;
 
 	for (int i = num_rounds; i > 0; i -= 2) {
-		chacha_rounds(x);
+		chacha_rounds(ctx->block.u32);
 	}
 
 	for (int i = 0; i < 16; ++i) {
-		U32TO8_LITTLE(output + 4 * i, x[i] + matrix[i]);
+		ctx->block.u32[i] += ctx->matrix.u32[i];
 	}
-}
-
-typedef struct {
-	uint64_t counter;
-	uint64_t nonce;
-	int used;
-	int rounds;
-	unsigned char block[64];
-	unsigned char key[32];
-} chacha_ctx_t;
-
-void init_chacha_matrix(chacha_ctx_t const * ctx, uint32_t matrix[16])
-{
-	unsigned char const sigma[] = {"expand 32-byte k"};
-
-	memcpy(matrix, sigma, 16);
-	memcpy(matrix+4, ctx->key, sizeof(ctx->key));
-	memcpy(matrix+14, &ctx->nonce, sizeof(ctx->nonce));
+	++ctx->matrix.u64[5];
 }
 
 void chacha_crypt(chacha_ctx_t * ctx, void * _buffer, size_t len)
@@ -88,42 +65,34 @@ void chacha_crypt(chacha_ctx_t * ctx, void * _buffer, size_t len)
 
 	/* Use up the remaining values in block. */
 	while (len > 0 && ctx->used & 0x3f) {
-		*(buffer++) ^= ctx->block[ctx->used++];
+		*(buffer++) ^= ctx->block.u8[ctx->used++];
 		--len;
-	}
-
-	uint32_t matrix[16];
-	if (len > 64) {
-		init_chacha_matrix(ctx, matrix);
 	}
 
 	/* If the block values have all been used, generate a new block */
 	if (64 == ctx->used) {
 		ctx->used = 0;
-		memcpy(matrix+12, &ctx->counter, sizeof(ctx->counter));
-		++ctx->counter;
-		ChaChaCore(ctx->block, matrix, ctx->rounds);
+		ChaChaCore(ctx, ctx->rounds);
 	}
 
 	/* Bulk xor the buffer with block and generate a new block */
 	for (; len >= 64; buffer += 64, len -= 64) {
-		*(uint64_t*)(buffer+0) ^= *(uint64_t*)(ctx->block+0);
-		*(uint64_t*)(buffer+8) ^= *(uint64_t*)(ctx->block+8);
-		*(uint64_t*)(buffer+16) ^= *(uint64_t*)(ctx->block+16);
-		*(uint64_t*)(buffer+24) ^= *(uint64_t*)(ctx->block+24);
-		*(uint64_t*)(buffer+32) ^= *(uint64_t*)(ctx->block+32);
-		*(uint64_t*)(buffer+40) ^= *(uint64_t*)(ctx->block+40);
-		*(uint64_t*)(buffer+48) ^= *(uint64_t*)(ctx->block+48);
-		*(uint64_t*)(buffer+56) ^= *(uint64_t*)(ctx->block+56);
-		memcpy(matrix+12, &ctx->counter, sizeof(ctx->counter));
-		++ctx->counter;
-		ChaChaCore(ctx->block, matrix, ctx->rounds);
+		uint64_t * b64 = (uint64_t*)buffer;
+		b64[0] ^= ctx->block.u64[0];
+		b64[1] ^= ctx->block.u64[1];
+		b64[2] ^= ctx->block.u64[2];
+		b64[3] ^= ctx->block.u64[3];
+		b64[4] ^= ctx->block.u64[4];
+		b64[5] ^= ctx->block.u64[5];
+		b64[6] ^= ctx->block.u64[6];
+		b64[7] ^= ctx->block.u64[7];
+		ChaChaCore(ctx, ctx->rounds);
 	}
 
 	/* xor the remaining value in the buffer and track how much
 	 * of the block is used in the process. */
 	while (len--) {
-		*(buffer++) ^= ctx->block[ctx->used++];
+		*(buffer++) ^= ctx->block.u8[ctx->used++];
 	}
 }
 
@@ -134,28 +103,29 @@ chacha_ctx_t * chacha_new_ctx(
 	unsigned char const * key,
 	size_t keylen,
 	uint64_t nonce,
-	int counter,
+	uint64_t counter,
 	int rounds,
 	int flags)
 {
-	chacha_ctx_t * retval = malloc(sizeof(chacha_ctx_t));
-	memset(retval, 0, sizeof(chacha_ctx_t));
-	if (key) {
-		memcpy(retval->key, key,
-			sizeof(retval->key) < keylen ? sizeof(retval->key) : keylen);
+	if (NULL == key) {
+		key = "";
 	}
-	retval->counter = counter;
-	retval->nonce = nonce;
-	retval->rounds = rounds;
+
+	chacha_ctx_t * ctx = malloc(sizeof(chacha_ctx_t));
+	memset(ctx, 0, sizeof(chacha_ctx_t));
+
+	ctx->rounds = rounds;
+
+	unsigned char const sigma[] = {"expand 32-byte k"};
+	memcpy(&ctx->matrix.u8[0], sigma, 16);
+	memcpy(&ctx->matrix.u8[16], key, 32 < strlen(key) ? 32 : strlen(key));
+	ctx->matrix.u64[6] = counter;
+	ctx->matrix.u64[7] = nonce;
 
 	/* Generate the first block */
-	uint32_t matrix[16];
-	init_chacha_matrix(retval, matrix);
-	memcpy(matrix+12, &retval->counter, sizeof(retval->counter));
-	++retval->counter;
-	ChaChaCore(retval->block, matrix, retval->rounds);
+	ChaChaCore(ctx, ctx->rounds);
 
-	return retval;
+	return ctx;
 }
 
 int main(int argc, char * argv[])
